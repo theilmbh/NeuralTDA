da0e81bbf23dfaa86c9a887f024677a21475be45
Brad Theilman <btheilma@ucsd.edu>
2016-05-13 08:22:47 -0700

    binned folder spec

--- neuraltda/calculate_CI_topology_from_binned.py
+++ neuraltda/calculate_CI_topology_from_binned.py
@@ -14,6 +14,7 @@ def get_args():
 	parser.add_argument('threshold', type=float, help= 'Threshold for Cell Groups')
 	parser.add_argument('block_path', type=str, help='Path to folder'
 													 'containing data files')
+	parser.add_argument('binned_path', type=str, help='Path to folder containing binned')
 
 	return parser.parse_args()
 
@@ -23,7 +24,7 @@ def main():
 
 	block_path = os.path.abspath(args.block_path)
 	analysis_id = args.analysis_id
-	binned_data_files = glob.glob(os.path.join(block_path, 'binned_data/*.binned'))
+	binned_data_files = glob.glob(binned_path)
 	
 	for bdf in binned_data_files:
 		topology.calc_CI_bettis_binned_data(analysis_id, bdf, block_path, args.threshold)
