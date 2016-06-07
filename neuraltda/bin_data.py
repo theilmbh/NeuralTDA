0f8257cdf91f2d8e83da06cfeb8a993a2322ee49
Brad Theilman <btheilma@ucsd.edu>
2016-06-07 10:53:28 -0700

    bin id

--- neuraltda/bin_data.py
+++ neuraltda/bin_data.py
@@ -13,6 +13,7 @@ def get_args():
 	parser.add_argument('block_path', type=str, help='Path to folder'
 													 'containing data files')
 	parser.add_argument('bin_def_file', type=str, help='location of the file describing the binning parameters')
+	parser.add_argument('bin_id', type=str, help='ID for binning')
 
 	return parser.parse_args()
 
@@ -21,7 +22,8 @@ def main():
 	args = get_args()
 	block_path = os.path.abspath(args.block_path)
 	bin_def_file = os.path.abspath(args.bin_def_file)
-	topology.prep_and_bin_data(block_path, bin_def_file)
+	bin_id = args.bin_id
+	topology.prep_and_bin_data(block_path, bin_def_file, bin_id)
 
 if __name__ == '__main__':
 	main()
