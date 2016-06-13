b19140aa3fe3f7b05f7f93a3e0a2d7c9d1c60a9e
Brad Theilman <btheilma@ucsd.edu>
2016-06-13 16:45:23 -0700

    changed rep reporting

--- neuraltda/topology.py
+++ neuraltda/topology.py
@@ -871,7 +871,7 @@ def calc_CI_bettis_binned_data(analysis_id, binned_data_file, block_path, thresh
 
             for rep in stim_trials.keys():
                 pfile = analysis_files_prefix + '-stim-{}'.format(stim) + \
-                    '-rep-{}'.format(int(rep)) + '-simplex.txt'
+                    '-rep-{}'.format(rep) + '-simplex.txt'
                 pfile = os.path.join(analysis_path, pfile)
                 bettis = calc_bettis_from_binned_data(stim_trials[rep], pfile, thresh)
 
