1432d0d603bc10de3e806d6e054db7aafa9bf373
Bradley Theilman <bradtheilman@gmail.com>
2016-07-12 17:19:42 -0700

    New console script for permuted

--- setup.py
+++ setup.py
@@ -15,7 +15,8 @@ setup(
             'bin_data = neuraltda.bin_data:main',
             'calc_CI_topology_binned = neuraltda.calculate_CI_topology_from_binned:main',
             'permute_data = neuraltda.permute_binned_data:main',
-            'shuffle_data = neuraltda.make_shuffled_binned_controls:main'
+            'shuffle_data = neuraltda.make_shuffled_binned_controls:main',
+            'calc_CI_topology_permuted_binned = neuraltda.calculate_CI_topology_from_permuted_binned:main'
         ],
     },
 )
