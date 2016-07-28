3fb357d7a291cdd85f31ac46f568b1d6af32089a
Bradley Theilman <bradtheilman@gmail.com>
2016-07-28 15:36:07 -0700

    Modifying buildspace

--- neuraltda/build_space.py
+++ neuraltda/build_space.py
@@ -3,17 +3,16 @@ import itertools
 import topology
 import numpy as np 
 
-def build_graph_recursive(graph, cell_group, parent_name, cluster_ids):
+def build_graph_recursive(graph, cell_group, parent_name,):
 
-
-	cells_in_group = cluster_ids[cell_group]
 	cell_group_name = ''.join(cell_group)
 	graph.add_node(cell_group_name)
 	if parent_name is not '':
 		graph.add_edge(cell_group_name, parent_name)
 	n_cells_in_group = len(cell_group)
-	for subgrp in itertools.combinations(cell_group, n_cells_in_group-1):
-		build_graph_recursive(graph, subgrp, cell_group_name, cluster_ids)
+	if n_cells_in_group > 1:
+		for subgrp in itertools.combinations(cell_group, n_cells_in_group-1):
+			build_graph_recursive(graph, subgrp, cell_group_name)
 
 	return graph
 
@@ -23,7 +22,7 @@ def build_graph_from_cell_groups(cell_groups):
 	graph = nx.Graph()
 	for win, group in cell_groups:
 		group_s = [str(s) for s in group]
-		graph = build_graph_recursive(graph, group_s, '', cluids)
+		graph = build_graph_recursive(graph, group_s, '')
 	return graph
 
 def build_graph_from_binned_dataset(binned_dataset, thresh):
@@ -31,4 +30,11 @@ def build_graph_from_binned_dataset(binned_dataset, thresh):
 	cell_groups = topology.calc_cell_groups_from_binned_data(binned_dataset, thresh)
 	graph = build_graph_from_cell_groups(cell_groups)
 	return graph 
+
+def build_graph_from_cell_groups_incremental(cell_groups, t):
+	graph = nx.Graph()
+	for win, group in cell_groups[:t]:
+		group_s = [str(s) for s in group]
+		graph = build_graph_recursive(graph, group_s, '')
+	return graph
 
