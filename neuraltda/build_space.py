d021adb6dc9627bf0065420148cd085486974563
Bradley Theilman <bradtheilman@gmail.com>
2016-07-29 13:07:34 -0700

    updates

--- neuraltda/build_space.py
+++ neuraltda/build_space.py
@@ -9,6 +9,7 @@ def build_graph_recursive(graph, cell_group, parent_name,):
 	graph.add_node(cell_group_name)
 	if parent_name is not '':
 		graph.add_edge(cell_group_name, parent_name)
+		graph.edge[cell_group_name][parent_name]['name'] = cell_group_name+parent_name
 	n_cells_in_group = len(cell_group)
 	if n_cells_in_group > 1:
 		for subgrp in itertools.combinations(cell_group, n_cells_in_group-1):
@@ -38,3 +39,11 @@ def build_graph_from_cell_groups_incremental(cell_groups, t):
 		graph = build_graph_recursive(graph, group_s, '')
 	return graph
 
+def compute_gamma_q(graph):
+
+	gamma_q = nx.Graph()
+	for (u,v, e_name) in graph.edges(data='name'):
+		gamma_q.add_node(e_name)
+		for (u_new, v_new, e_new_name) in nx.edges(graph, [u, v]):
+			gamma_q.add_edge(e_name, e_new_name)
+	return gamma_q 
