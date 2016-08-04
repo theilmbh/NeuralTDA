bebc24145532bf9f9bf61c56778274da3072605b
Bradley Theilman <bradtheilman@gmail.com>
2016-08-04 15:18:03 -0700

    modifications to hdms.. still not there

--- neuraltda/H_MDS.py
+++ neuraltda/H_MDS.py
@@ -11,10 +11,10 @@ def d_H(x1, x2):
 def ddH_dx1_1(x1, x2):
 
 	x1_1 = np.real(x1)
-	x1_2 = np.imaginary(x1)
+	x1_2 = np.imag(x1)
 
 	x2_1 = np.real(x2)
-	x2_2 = np.imaginary(x2)
+	x2_2 = np.imag(x2)
 
 	v1 = x1_1 - x2_1
 	v3 = x1_1*x2_1 + x1_2*x2_2 - 1
@@ -29,10 +29,10 @@ def ddH_dx1_1(x1, x2):
 def ddH_dx1_2(x1, x2):
 
 	x1_1 = np.real(x1)
-	x1_2 = np.imaginary(x1)
+	x1_2 = np.imag(x1)
 
 	x2_1 = np.real(x2)
-	x2_2 = np.imaginary(x2)
+	x2_2 = np.imag(x2)
 
 	v1 = x1_1 - x2_1
 	v3 = x1_1*x2_1 + x1_2*x2_2 - 1
@@ -50,9 +50,9 @@ def mobius_xform(z, c, theta):
 	b = np.conj(c)*theta*z + 1
 	return a/b
 
-def E_x(X, D, w):
+def E_x(X, Y, D, w):
 
-	d = get_distances(X)
+	d = get_distances(X, Y)
 	diffmat = np.subtract(d, D)
 	diffmat = np.square(diffmat)
 	w = np.triu(w, k=1)
@@ -62,7 +62,7 @@ def E_x(X, D, w):
 def get_distances(X, Y):
 
 	dmat = np.zeros((len(X), len(X)))
-	for i in range(len(X))
+	for i in range(len(X)):
 		for k in range(i):
 			dmat[k, i] = d_H(X[i]+1j*Y[i], X[k]+1j*Y[k])
 	return dmat + np.transpose(dmat)
@@ -78,16 +78,17 @@ def dE_dxa(D, w, X, Y, alpha, q):
 		ddH = lambda x, y: ddH_dx1_1(x, y)
 	elif q==2:
 		ddH = lambda x, y: ddH_dx1_2(x, y)
-
-	for j in range(len(X)):
+	rng = range(len(X))
+	rng = rng[alpha+1:]
+	for j in rng:
 		x_j = X[j] + 1j*Y[j]
-		dd = ddH[x_a, x_j]
+		dd = ddH(x_a, x_j)
 		dd_vec[j] = np.real(dd)
 
-
+	
 	w = np.triu(w, k=1)
 	w_j = w[alpha, :]
-	diffmat = np.subtract(d[alpha, :] - D[alpha, :])
+	diffmat = np.subtract(d[alpha, :],  D[alpha, :])
 	dEdxa = 2*np.einsum('j,j,j', w_j, diffmat, dd_vec)
 	return dEdxa
 
@@ -114,8 +115,9 @@ def HMDS_update(D, w, X, Y, alpha, eta):
 	delta_r = dE_1 / np.abs(ddEdxa1)
 	delta_i = dE_2 / np.abs(ddEdxa2)
 
-	delta = eta*(delta_r +1j*delta_i) 
-	X[alpha] = mobius_xform(X[alpha], delta, 1)
+	delta = -1.0*eta*(delta_r +1j*delta_i) 
+	new = mobius_xform(X[alpha]+1j*Y[alpha], delta, 1)
+	return new
 
 
 
