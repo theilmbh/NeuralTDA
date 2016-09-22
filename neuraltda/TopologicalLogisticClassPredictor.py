26578b693c3eeb60fdc2106481ae7a36561b7bcf
Bradley Theilman <bradtheilman@gmail.com>
2016-09-22 13:17:17 -0700

    predictor

---
+++ neuraltda/TopologicalLogisticClassPredictor.py
@@ -0,0 +1,56 @@
+import numpy as np
+import topology as tp
+import pickle
+
+class TopologicalLogisticClassPredictor:
+
+	def __init__(self, masterResults, stimuliClasses, **kwargs):
+
+		self.stimClasses = stimuliClasses
+		with open(masterResults, 'r') as f:
+			self.resultsDict = pickle.load(f)
+
+		self.trainedStimuliData = {}
+
+	def extractTrainedStims(self, computationClass):
+
+		clData = self.resultsDict[computationClass]
+		for stim in clData.keys():
+			if stim in self.stimuliClasses.keys():
+				self.trainedStimuliData[stim] = clData[stim]
+
+	def bptd_recursive(self, bpd, bpdf):
+
+    	if 'hstr' in bpd.keys():
+        	barcode = bpd['barcodes']
+        	nfilt = len(bettis)
+        	betti_dict = dict()
+        	for dimindx, dim in barcode.keys():
+            	nPersist = sum([-1 in s for s in barcode[dim]])
+                betti_dict[dim] = betti_num
+            betti_dict['hierarchy'] = bpd['hstr']
+            filtdataframe = pd.DataFrame(data=betti_dict, index=[1])
+            bpdf = bpdf.append(filtdataframe, ignore_index=True)
+        	return bpdf
+    	else:
+        	for indx, h_level in enumerate(bpd.keys()):
+            	bpdf = self.bptd_recursive(bpd[h_level], bpdf)
+        	return bpdf
+
+    def getStimClass(self, row):
+
+    	hstr = row['hierarchy']
+    	for stim in self.stimClasses.keys():
+    		if stim in hstr:
+    			return self.stimClasses[stim]
+
+
+	def buildPredictionDataMatrix(self):
+
+		self.persistentBettiFrame = pd.DataFrame(columns=['hierarchy','0', '1', '2'])
+		self.persistentBettiFrame = self.bptd_recursive(self.trainedStimuliData, self.persistentBettiFrame)
+		self.persistentBettiFrame['stimClass'] = self.persistentBettiFrame.apply(lambda row: self.getStimClass(row), axis=1)
+
+
+
+
