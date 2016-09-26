daa8e4372901d41353e6b1f58a55bf16709ecfbf
Bradley Theilman <bradtheilman@gmail.com>
2016-09-26 13:54:12 -0700

    reformulate

--- neuraltda/TopologicalLogisticClassPredictor.py
+++ neuraltda/TopologicalLogisticClassPredictor.py
@@ -4,6 +4,7 @@ import pickle
 import pandas as pd
 import patsy
 import statsmodels.api as sm 
+import itertools
 
 class TopologicalLogisticClassPredictor:
 
@@ -12,8 +13,12 @@ class TopologicalLogisticClassPredictor:
         self.stimClasses = stimuliClasses
         with open(masterResults, 'r') as f:
             self.resultsDict = pickle.load(f)
-
+        self.predMaxBetti = 5
+        self.predNTimes = 10
         self.trainedStimuliData = {}
+        self.persistentBettiArray = []
+        self.predClassArray = []
+        self.colnames = ['B%d_T%d' % s for s in itertools.product(range(self.predMaxBetti), range(self.predNTimes))]
 
     def extractTrainedStims(self, computationClass):
 
@@ -40,6 +45,38 @@ class TopologicalLogisticClassPredictor:
                 bpdf = self.bptd_recursive(bpd[h_level], bpdf)
             return bpdf
 
+    def buildPredictionDataMatrixRecursive(self, bpd):
+
+        if 'hstr' in bpd.keys():
+            # We are at bottom of hierarchy
+            bettiData = bpd['bettis']
+            curveStore = np.zeros((self.predMaxBetti, self.predNTimes))
+            filts = [s[0] for s in bettiData]
+            bettis = [s[1] for s in bettiData]
+            t = np.linspace(min(filts), max(filts), self.predNTimes)
+            for dim in range(self.predMaxBetti):
+                try:
+                    bettiVals = [s[1][dim] for s in bettiData]
+                except IndexError:
+                    bettiVals = [0 for s in bettiData]
+                bettifunc = interp1d(filts, bettiVals, kind='zero',
+                                     bounds_error=False,
+                                     fill_value=(bettiVals[0], bettiVals[-1]))
+                bettiCurve = bettifunc(t)
+                curveStore[dim, :] = bettiCurve
+            bettiDataRow = np.reshape(curveStore, (1, self.predMaxBetti*self.predNTimes))
+            self.persistentBettiArray.append(bettiDataRow)
+            newDF = pd.DataFrame(data=bettiDataRow, columns=colnames, index=[1])
+            self.persistentBettiFrame = self.persistentBettiFrame.append(bettiDataRow, ignore_index=True)
+            hstr = bpd['hstr']
+            for stim in self.stimClasses.keys():
+                if stim in hstr:
+                    stmcls = self.stimClasses[stim]
+                    self.predClassArray.append(stmcls)
+        else:
+            for indx, hLevel in enumerate(bpd.keys()):
+                self.buildPredictionDataMatrixRecursive(bpd)
+
     def getStimClass(self, row):
 
         hstr = row['hierarchy']
@@ -47,12 +84,11 @@ class TopologicalLogisticClassPredictor:
             if stim in hstr:
                 return self.stimClasses[stim]
 
-
     def buildPredictionDataMatrix(self):
 
-        self.persistentBettiFrame = pd.DataFrame(columns=['hierarchy','B0', 'B1', 'B2', 'B3', 'B4', 'B5', 'B6'])
-        self.persistentBettiFrame = self.bptd_recursive(self.trainedStimuliData, self.persistentBettiFrame)
-        self.persistentBettiFrame['stimClass'] = self.persistentBettiFrame.apply(lambda row: self.getStimClass(row), axis=1)
+        self.persistentBettiFrame = pd.DataFrame(columns=self.colnames)
+        self.buildPredictionDataMatrixRecursive(self.trainedStimuliData)
+        self.persistentBettiFrame['stimClass'] = self.predClassArray
 
     def formatModelInput(self):
 
