fa7bac0a653fd7a156478a49384804bd76c6057c
Bradley Theilman <bradtheilman@gmail.com>
2016-09-29 10:35:13 -0700

    debug

--- neuraltda/FiringRateLogisticClassPredictor.py
+++ neuraltda/FiringRateLogisticClassPredictor.py
@@ -29,6 +29,7 @@ class FiringRateLogisticClassPredictor:
         if 'pop_vec' in bpd.keys():
             # We are at bottom of hierarchy
             popVec = np.array(bpd['pop_vec'])
+            print(popVec.shape)
             avgFRVec = np.transpose(np.mean(popVec, 1))
             print(avgFRVec.shape)
             self.FRVecArray.append(avgFRVec)
