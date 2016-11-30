20bd49f80e27ab348e239733501d8f971cd95e45
Brad Theilman <btheilma@ucsd.edu>
2016-11-29 18:28:03 -0800

    Removed excess code

--- neuraltda/topology2.py
+++ neuraltda/topology2.py
@@ -22,7 +22,6 @@ from ephys import events, core
 ################################
 
 TOPOLOGY_LOG = logging.getLogger('NeuralTDA')
-
 DEFAULT_CG_PARAMS = {'cluster_group': None, 'subwin_len': 100,
                      'threshold': 6.0, 'n_subwin': 5}
 
@@ -98,46 +97,6 @@ def get_spikes_in_window(spikes, window):
             (spikes['time_samples'] >= window[0]))
     return spikes[mask]
 
-def calc_mean_fr(cluster_row, spikes, window):
-    '''
-    Computes the mean firing rate of a unit within the given spikes DataFrame
-
-    Parameters
-    ----------
-    row : pandas dataframe row
-        Row of clusters dataframe containing clusterID to compute
-        (see ephys.clusters)
-    spikes : pandas dataframe
-        Contains the spike data (see ephys.core).
-        Firing rate computed from this data over the window
-    window : tuple
-        Time, in samples, over which the spikes in 'spikes' occur.
-
-    Returns
-    -------
-    mean_fr : float
-        Mean firing rate over the interval.
-    '''
-    cluster = cluster_row['cluster']
-    spikes = spikes[spikes['cluster'] == cluster]
-    spikes = get_spikes_in_window(spikes, window)
-    nspikes = len(spikes.index)
-    dt = window[1] - window[0]
-    mean_fr = (1.0*nspikes) / dt
-    retframe = pd.DataFrame({'mean_fr': [mean_fr]})
-    return retframe.iloc[0]
-
-def calc_mean_fr_int(cluster, spikes, window):
-    ''' Does the same as above, but for ints.  This is bad.  Fix this
-    '''
-    spikes = spikes[spikes['cluster'] == cluster]
-    spikes = get_spikes_in_window(spikes, window)
-    nspikes = len(spikes.index)
-    dt = window[1] - window[0]
-    mean_fr = (1.0*nspikes) / dt
-    return mean_fr
-
-
 def create_subwindows(segment, subwin_len, noverlap=0):
     '''
     Create list of subwindows for cell group identification.
@@ -165,141 +124,6 @@ def create_subwindows(segment, subwin_len, noverlap=0):
     starts = [segment[0] + k*skip for k in range(maxK)]
     windows = [[w, min(w + (subwin_len-1), segment[1])] for w in starts]
     return windows
-
-def calc_population_vectors(spikes, clusters, windows, thresh):
-    '''
-    Builds population vectors according to Curto and Itskov 2008.
-
-    Parameters
-    ------
-    spikes : pandas DataFrame
-        DataFrame containing spike data.
-        Must have 'fr_mean' column containing mean firing rate.
-    clusters : pandas DataFrame
-        Dataframe containing cluster information.
-    windows : tuple
-        The set of windows for which to compute population vectors.
-    thresh : float
-        Multiple of mean firing rate for a cluster to be included in vector.
-
-    Returns
-    ------
-    popvec_list : list
-        Population vector list.
-        Each element is a list containing the window and the population vector.
-        The population vector is an array containing cluster ID and firing rate.
-    '''
-    TOPOLOGY_LOG.info('Building population vectors...')
-    total_win = len(windows)
-    popvec_list = []
-    for win_num, win in enumerate(windows):
-        if np.mod(win_num, 500) == 0:
-            TOPOLOGY_LOG.info("Window {} of {}".format(str(win_num),
-                                                       str(total_win)))
-        popvec = np.zeros([len(clusters.index), 3])
-        for ind, cluster in enumerate(clusters['cluster'].values):
-            fr = calc_mean_fr_int(cluster, spikes, win)
-            cluster_fr = clusters[clusters['cluster'] == cluster]
-            fr_mask = (1.0*thresh*cluster_fr['mean_fr']).values
-            fr_vec = fr > fr_mask
-            popvec[ind, 1] = fr
-            popvec[ind, 0] = cluster
-            popvec[ind, 2] = fr_vec
-        popvec_list.append([win, popvec])
-    return popvec_list
-
-def calc_cell_groups(spikes, segment, clusters, cg_params=DEFAULT_CG_PARAMS):
-    '''
-    Creates cell group DataFrame according to Curto and Itskov 2008
-
-    Parameters
-    ----------
-    spikes : pandas dataframe
-        Dataframe containing the spikes to analyze.
-    segment : tuple or list of floats
-        Time window for which to create cell groups.
-    clusters : pandas DataFrame
-        DataFrame containing cluster information
-    cg_params : dict, optional
-        Parameters for cell group creation.  Includes:
-        cluster_group : str
-            Quality of the clusters to include in the analysis (Good, MUA, etc)
-        subwin_len : int
-            length (samples) for each subwin
-        threshold : float, optional
-            Multiples above baseline firing rate to include activity
-        n_subwin : int
-            Number of subwindows to use to generate population vectors
-
-    Returns
-    -------
-    cell_groups : list
-        list where each entry is a list containing a time window
-        and the ID's of the cells in that group
-    '''
-    cluster_group = cg_params['cluster_group']
-    subwin_len = cg_params['subwin_len']
-    threshold = cg_params['threshold']
-    n_subwin = cg_params['n_subwin']
-
-    spikes = get_spikes_in_window(spikes, segment)
-    if cluster_group != None:
-        mask = np.ones(len(clusters.index)) < 0
-        for grp in cluster_group:
-            mask = np.logical_or(mask, clusters['quality'] == grp)
-        clusters = clusters[mask]
-        spikes = spikes[spikes['cluster'].isin(clusters['cluster'].values)]
-
-    topology_subwindows = create_subwindows(segment, subwin_len, n_subwin)
-    mean_fr_row_func = lambda row: calc_mean_fr(row, spikes, segment)['mean_fr']
-    clusters['mean_fr'] = clusters.apply(mean_fr_row_func, axis=1)
-
-    # Build population vectors
-    population_vector_list = calc_population_vectors(spikes, clusters,
-                                                     topology_subwindows,
-                                                     threshold)
-    cell_groups = []
-    for population_vector_win in population_vector_list:
-        win = population_vector_win[0]
-        popvec = population_vector_win[1]
-        active_cells = popvec[(popvec[:, 2].astype(bool)), 0].astype(int)
-        cell_groups.append([win, active_cells])
-
-    return cell_groups
-
-def build_perseus_input(cell_groups, savefile):
-    '''
-    Formats cell group information as an input file
-    for the Perseus persistent homology software
-
-    Parameters
-    ------
-    cell_groups : list
-        cell_group information returned by calc_cell_groups
-    savefile : str
-        File in which to put the formatted cellgroup information
-
-    Yields
-    ------
-    savefile : text File
-        file suitable for running perseus on
-    '''
-    with open(savefile, 'w+') as pfile:
-        pfile.write('1\n')
-        for win_grp in cell_groups:
-            grp = list(win_grp[1])
-            #debug_print('Cell group: ' + str(grp) +'\n')
-            grp_dim = len(grp) - 1
-            if grp_dim < 0:
-                continue
-            vert_str = str(grp)
-            vert_str = vert_str.replace('[', '')
-            vert_str = vert_str.replace(']', '')
-            vert_str = vert_str.replace(' ', '')
-            vert_str = vert_str.replace(',', ' ')
-            out_str = str(grp_dim) + ' ' + vert_str + ' 1\n'
-            pfile.write(out_str)
-    return savefile
 
 def build_perseus_persistent_input(cell_groups, savefile):
     '''
@@ -363,50 +187,6 @@ def run_perseus(pfile):
     TOPOLOGY_LOG.info('Betti file from run_perseus: %s' % betti_file)
     return betti_file
 
-def calc_bettis(spikes, segment, clusters, pfile, cg_params=DEFAULT_CG_PARAMS,
-                persistence=False):
-    '''
-    Calculate betti numbers for spike data in segment.
-
-    Parameters
-    ------
-    spikes : pandas DataFrame
-        Dataframe containing spike data
-    segment : list
-        Time window, in samples, of data to calculate betti numbers
-    clusters : pandas Dataframe
-        Dataframe containing cluster data
-    pfile : str
-        File in which to save simplex data
-    cg_params : dict
-        Parameters for cell group generation
-    persistence : bool, optional
-        If true, will compute the time dependence of the bettis
-        by including filtration times in the cell groups
-
-    Returns
-    ------
-    bettis : list
-        Betti numbers.  Each member is a list with the first member being
-        filtration time and the second being betti numbers
-    '''
-    cell_groups = calc_cell_groups(spikes, segment, clusters, cg_params)
-    if persistence:
-        build_perseus_persistent_input(cell_groups, pfile)
-    else:
-        build_perseus_input(cell_groups, pfile)
-    betti_file = run_perseus(pfile)
-    bettis = []
-    with open(betti_file, 'r') as bf:
-        for bf_line in bf:
-            if len(bf_line) < 2:
-                continue
-            betti_data = bf_line.split()
-            filtration_time = int(betti_data[0])
-            betti_numbers = map(int, betti_data[1:])
-            bettis.append([filtration_time, betti_numbers])
-    return bettis
-
 def get_segment(trial_bounds, fs, segment_info):
     '''
     Convert a segment info specifier into a segment.
@@ -442,47 +222,6 @@ def get_segment(trial_bounds, fs, segment_info):
 ###### Binned Data Auxiliary Functions ######
 #############################################
 
-def calc_cell_groups_from_binned_data(binned_dataset, thresh):
-    '''
-    Given a binned dataset and a firing rate threshold,
-    generate the time sequence of cell groups.
-
-    Parameters
-    ----------
-    binned_dataset : h5py group
-        This is the group at the lowest level of the bin hierarchy.
-        It must contain three datasets: 'pop_vec', 'clusters', and 'windows'.
-        - 'pop_vec' is an nclu x nwin matrix with each element containing
-          the firing rate in Hz of that unit for that window.
-        - 'clusters' is an nclu x 1 matrix containing the cluster ids of each
-          row of 'pop_vec'
-        - 'windows' is an nwin x 2 matrix containing the start and end samples
-          of each window used for the binning.
-    thresh : float
-        Multiple of average firing rate in order for a cluster to be included
-        in a cell group.
-
-    Returns
-    -------
-    cell_groups : list
-        A list containing entries for each cell group.
-        The first element of each entry is the window number of the cell group.
-        The second element of each entry is a list of cluster ids for the cells
-        in the cell group.
-    '''
-    bds = np.array(binned_dataset['pop_vec'])
-    clusters = np.array(binned_dataset['clusters']).astype(int)
-    nwin = bds.shape[1]
-    mean_frs = np.mean(bds, 1)
-    cell_groups = []
-    for win in range(nwin):
-        acty = bds[:, win]
-        above_thresh = np.greater(acty, thresh*mean_frs)
-        clus_in_group = clusters[above_thresh]
-        cell_groups.append([win, clus_in_group])
-    return cell_groups
-
-
 def calcCellGroups(data_mat, clusters, thresh):
     '''
     Given a matrix of firing rates, generate time sequence of cell groups
@@ -503,53 +242,6 @@ def calcCellGroups(data_mat, clusters, thresh):
         clus_in_group = clusters[above_thresh[:, win]]
         cell_groups.append([win, clus_in_group])
     return cell_groups 
-
-def calc_bettis_from_binned_data(binned_dataset, pfile, thresh):
-    '''
-    Calculates betti numbers from a previously binned dataset.
-
-    Parameters
-    ----------
-    binned_dataset : h5py group
-        This is the group at the lowest level of the bin hierarchy.
-        It must contain three datasets: 'pop_vec', 'clusters', and 'windows'.
-        - 'pop_vec' is an nclu x nwin matrix with each element containing
-          the firing rate in Hz of that unit for that window.
-        - 'clusters' is an nclu x 1 matrix containing the cluster ids of each
-          row of 'pop_vec'
-        - 'windows' is an nwin x 2 matrix containing the start and end samples
-          of each window used for the binning.
-    pfile : str
-        The name of the file to store the Perseus input.
-    thresh : float
-        Multiple of average firing rate in order for a cluster to be included
-        in a cell group.
-
-    Returns
-    -------
-    bettis : list
-        List containing betti numbers.
-        The first entry in each element is the filtration time point.
-        The second entry in each element is a list containing the values of the
-        betti numbers for each dimension.
-    '''
-    cell_groups = calc_cell_groups_from_binned_data(binned_dataset, thresh)
-    build_perseus_persistent_input(cell_groups, pfile)
-    betti_file = run_perseus(pfile)
-    bettis = []
-    try:
-        with open(betti_file, 'r') as bf:
-            for bf_line in bf:
-                if len(bf_line) < 2:
-                    continue
-                betti_data = bf_line.split()
-                filtration_time = int(betti_data[0])
-                betti_numbers = map(int, betti_data[1:])
-                bettis.append([filtration_time, betti_numbers])
-    except:
-        bettis.append([-1, [-1]])
-        TOPOLOGY_LOG.warn('Perseus returned invalid betti file')
-    return bettis
 
 def calcBettis(data_mat, clusters, pfile, thresh):
     '''
@@ -615,7 +307,6 @@ def calcCIBettisTensor(analysis_id, binned_data_file,
     TOPOLOGY_LOG.info('Theshold: {}'.format(thresh))
 
     with h5py.File(binned_data_file, 'r') as bdf:
-
         stims = bdf.keys()
         nstims = len(stims)
         bpd_withstim = dict()
@@ -744,6 +435,10 @@ def build_population_embedding_tensor(spikes, trials, clusters, win_size, fs,
                             win_s = win_size/1000.
                             poptens_dset[pvclu_msk, win_ind, rep] = nsp_clu/win_s
 
+##############################
+###### Computation Dags ######
+##############################
+
 def dbLoadData(block_path):
 
     # Load Raw Data
@@ -786,7 +481,6 @@ def dag_topology(block_path, thresh, bfdict, simplexWinSize=0):
     aid = bfdict['analysis_id']
     bpt = os.path.join(block_path, 'topology/')
     analysis_dict = dict()
-
     if 'raw' in bfdict.keys():
         rawFolder = bfdict['raw']
         tpid_raw = aid +'-{}-raw'.format(thresh)
