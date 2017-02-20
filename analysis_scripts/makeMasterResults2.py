e6d248de0c26838279bb8cbcf7f36c42fdd4e28e
Bradley Theilman <bradtheilman@gmail.com>
2017-02-20 07:35:57 -0800

    params

--- analysis_scripts/makeMasterResults2.py
+++ analysis_scripts/makeMasterResults2.py
@@ -1,13 +1,13 @@
 import neuraltda.topology2 as tp2
 
 blockPath='./'
-winSize = 25.0
+winSize = 15.0
 segmentInfo = {'period':1}
 ncellsperm = 30
 nperms = 20
 nshuff = 1
-thresh=4.0
+thresh = 5.0
 
 tp2.setup_logging('B1083_P03S05_25ms_4x')
 bfdict = tp2.dag_bin(blockPath, winSize, segmentInfo, dtOverlap=dtovr)
-tp2.dag_topology(blockPath, thresh, bfdict, raw=True, shuffle=True, shuffleperm=True, nperms=nperms, ncellsperm=ncellsperm)
+tp2.dag_topology(blockPath, thresh, bfdict, raw=False, shuffle=False, shuffleperm=True, nperms=nperms, ncellsperm=ncellsperm)
