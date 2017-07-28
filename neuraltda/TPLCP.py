94bb2c508b9f73548945051415ed26a01715a735
Brad Theilman <btheilma@ucsd.edu>
2017-07-28 10:32:19 -0700

    arbitrary unfamiliar

--- neuraltda/TPLCP.py
+++ neuraltda/TPLCP.py
@@ -29,6 +29,44 @@ def predict_stimuli_classes(betti_curves, npercurve, stimuli, stim_classes, pc_t
 
     return accuracies
 
+def assign_arbitary_classes(stimuli, class_labels):
+
+    n_labels = len(class_labels)
+
+    assert len(stimuli) % n_labels == 0
+    stride = int(len(stimuli) / n_labels)
+    perms = np.random.permutation(np.arange(len(stimuli)))
+    stimuli_classes = {}
+    for labn in range(n_labels):
+        stim_num = perms[(labn*stride):(labn*stride)+stride]
+        for num in stim_num:
+            stimuli_classes[stimuli[num]] = class_labels[labn]
+    return stimuli_classes
+
+def predict_arbitrary_classes(betti_curves, stimuli, stim_class_labels, pc_test, n_predict, shuff_Y=False):
+
+        (ndims, ntimes, ntrials) = betti_curves[list(betti_curves.keys())[0]].shape
+
+        accuracies = []
+        for pred in range(n_predict):
+            pred_y = np.array([])
+            pred_x = np.empty((0, ndims*ntimes))
+            stim_classes = assign_arbitary_classes(list(stimuli), stim_class_labels)
+            for stim in stimuli:
+                dat = np.reshape(betti_curves[stim], (ndims*ntimes, ntrials)).T
+                stim_vec = np.array(ntrials*[stim_classes[stim]])
+                pred_y = np.hstack([pred_y,stim_vec])
+                pred_x = np.vstack([pred_x, dat])
+
+            pred_Y = np.array(pred_y)
+            pred_X = np.array(pred_x)
+
+            if shuff_Y:
+                pred_Y = np.random.permutation(pred_Y)
+            accuracies.append(run_prediction(pred_Y, pred_X, pc_test));
+
+        return accuracies
+
 def run_prediction(pred_Y, pred_X, pc_test):
     total_pts = len(pred_Y)
     ntrain = int(np.round((1-pc_test)*total_pts))
