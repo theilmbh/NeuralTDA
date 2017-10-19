7f28e4f2b2c62f94ae0459b570b4d6bbafc09c2c
Brad Theilman <btheilma@ucsd.edu>
2017-10-18 21:15:41 -0700

    slse header

---
+++ c_code/slse.h
@@ -0,0 +1,28 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  slse.h
+ *
+ *    Description:  Simplicial laplacian spectral entropy
+ *
+ *        Version:  1.0
+ *        Created:  10/18/2017 10:30:09 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Brad Theilman (BHT), bradtheilman@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#ifndef SLSE_H
+#define SLSE_H
+
+#include <gsl/gsl_matrix.h>
+
+int check_square_matrix(gsl_matrix * a);
+double KL_divergence(gsl_matrix * L1, gsl_matrix * L2, double beta);
+
+
+#endif
